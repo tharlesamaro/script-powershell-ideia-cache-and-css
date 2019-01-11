a1b9c74456814ad05570a2c9b407d7f6cdf0a79c
Tharles Amaro <tharlesamaro@gmail.com>
2019-01-11 20:52:46 -0300

    refatoraç

--- rotinasAutomatizadas.ps1
+++ rotinasAutomatizadas.ps1
@@ -1,4 +1,10 @@
-﻿Write-Host ""
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+
+Write-Host ""
 Write-Host "**************************************************************************************"
 Write-Host "* .___    .___     .__           _________               __                          *"
 Write-Host "* |   | __| _/____ |__|____     /   _____/__.__. _______/  |_  ____   _____   ______ *"
@@ -10,154 +16,53 @@ Write-Host "*                                                                   
 Write-Host "**************************************************************************************"
 Write-Host ""
 
+$server = "\\SRVIDEIA\Dados\allCSS\"
+
+$pathDestinyCSSResource = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\Resource"
+$pathDestinyCSSResourceClasses = $pathDestinyCSSCityBusResource + "\classes.css"
+
+$pathDestinyCSSPortalLight = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\Skins\PortalLight"
+$pathDestinyCSSPortalLightClasses = $pathDestinyCSSCityBusPortalLight + "\classes.css"
+
+$pathDestinyCSSHTMLGrid = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\Skins\PortalLight\HTMLGrid"
+$pathdestinyCSSHTMLGridFile = $pathDestinyCSSHTMLGrid + "\grid.css"
+
 $action = "-1"
 
-DO {
+function delete-file-or-directory-if-exists
+{
+    Param ([string]$fileOrDirectory)
 
-    $action = Read-Host -Prompt "Digite: (1) Limpar cache | (2) CSS CityBus | (3) CSS Financeiro | (4) CSS Geope | (0) Sair"
-
-    $server = "\\SRVIDEIA\Dados\allCSS\"
-
-    $pathDestinyCSSResource = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\Resource"
-    $pathDestinyCSSResourceClasses = $pathDestinyCSSCityBusResource + "\classes.css"
-
-    $pathDestinyCSSPortalLight = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\Skins\PortalLight"
-    $pathDestinyCSSPortalLightClasses = $pathDestinyCSSCityBusPortalLight + "\classes.css"
-
-    $pathDestinyCSSHTMLGrid = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\Skins\PortalLight\HTMLGrid"
-    $pathdestinyCSSHTMLGridFile = $pathDestinyCSSHTMLGrid + "\grid.css"
-
-    if ( $action -eq "1" ) {
-
-        $pathSaved = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\config\saved\*"
-        $pathCatalina = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\work\Catalina"
-        $pathCache = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\cache\*"
-        $pathWebrunstudio = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\cacheCompressed\webrunstudio\*"
-
-        $getChildPathSaved = Get-ChildItem $pathSaved | Measure-Object
-        $getChildPathCache = Get-ChildItem $pathCache | Measure-Object
-        $getChildPathWebrunstudio = Get-ChildItem $pathWebrunstudio | Measure-Object
-
-
-        if ( $getChildPathSaved.count -ge 1 ) {
-            Remove-Item -path $pathSaved -recurse
-        }
-
-        if ( Test-Path -Path $pathCache ) {
-            Remove-Item -path $pathCatalina -recurse
-        }
-
-        if ( $getChildPathCache.count -ge 1 ) {
-            Remove-Item -path $pathCache -recurse
-        }
-
-        if ( $getChildPathWebrunstudio.count -ge 1 ) {
-            Remove-Item -path $pathWebrunstudio -recurse
-        }
+    if ( Test-Path -Path $fileOrDirectory ) {
+        Remove-Item -path $fileOrDirectory -recurse
     }
+}
 
-    if ( $action -eq "2" ) {
-    
-        $pathOriginCSSCityBusResourceClasses = $server + "citybus\resource\classes.css"
-        $pathOriginCSSCityBusPortalLightClasses = $server + "citybus\classes.css"
-        $pathOriginCSSCityBusGrid = $server + "citybus\grid.css"
+function clean-directory
+{
+    Param ([String]$directory)
 
-        if ( Test-Path -Path $pathOriginCSSCityBusResourceClasses ) {
+    $getChildOfDirectory = Get-ChildItem $directory | Measure-Object
 
-            if ( Test-Path -Path $pathDestinyCSSResourceClasses ) {
-                Remove-Item -path $pathDestinyCSSResourceClasses -recurse
-            }
-
-            Copy-Item $pathOriginCSSCityBusResourceClasses -Destination $pathDestinyCSSResource
-        }
-
-        if ( Test-Path -Path $pathOriginCSSCityBusPortalLightClasses ) {
-        
-            if ( Test-Path -Path $pathDestinyCSSPortalLightClasses ) {
-                Remove-Item -path $pathDestinyCSSPortalLightClasses -recurse
-            }
-
-            Copy-Item $pathOriginCSSCityBusPortalLightClasses -Destination $pathDestinyCSSPortalLight
-        }
-
-        if ( Test-Path -Path $pathOriginCSSCityBusGrid ) {
-
-            if ( Test-Path -Path $pathdestinyCSSHTMLGridFile ) {
-                Remove-Item -path $pathdestinyCSSHTMLGridFile -recurse
-            }
-
-            Copy-Item $pathOriginCSSCityBusGrid -Destination $pathDestinyCSSHTMLGrid  
-        }
+    if ( $getChildOfDirectory.count -ge 1 ) {
+        Remove-Item -path $directory -recurse
     }
+}
 
-    if ( $action -eq "3" ) {
-    
-        $pathOriginCSSFinanceiroResourceClasses = $server + "financeiro\resource\classes.css"
-        $pathOriginCSSFinanceiroPortalLightClasses = $server + "financeiro\classes.css"
-        $pathOriginCSSFinanceiroGrid = $server + "financeiro\grid.css"
+function copy-file-to-directory
+{
+    Param([String]$newFile, [String]$destinationDirectory, [String]$oldFile)
 
-        if ( Test-Path -Path $pathOriginCSSFinanceiroResourceClasses ) {
+    if ( Test-Path -Path $newFile ) {
 
-            if ( Test-Path -Path $pathDestinyCSSResourceClasses ) {
-                Remove-Item -path $pathDestinyCSSResourceClasses -recurse
-            }
+        delete-file-or-directory-if-exists -fileOrDirectory $oldFile
 
-            Copy-Item $pathOriginCSSFinanceiroResourceClasses -Destination $pathDestinyCSSResource
-        }
-
-        if ( Test-Path -Path $pathOriginCSSFinanceiroPortalLightClasses ) {
-        
-            if ( Test-Path -Path $pathDestinyCSSPortalLightClasses ) {
-                Remove-Item -path $pathDestinyCSSPortalLightClasses -recurse
-            }
-
-            Copy-Item $pathOriginCSSFinanceiroPortalLightClasses -Destination $pathDestinyCSSPortalLight
-        }
-
-        if ( Test-Path -Path $pathOriginCSSFinanceiroGrid ) {
-
-            if ( Test-Path -Path $pathdestinyCSSHTMLGridFile ) {
-                Remove-Item -path $pathdestinyCSSHTMLGridFile -recurse
-            }
-
-            Copy-Item $pathOriginCSSFinanceiroGrid -Destination $pathDestinyCSSHTMLGrid  
-        }
+        Copy-Item $newFile -Destination $destinationDirectory
     }
+}
 
-    if ( $action -eq "4" ) {
-
-        $pathOriginCSSGeopeResourceClasses = $server + "geope\resource\classes.css"
-        $pathOriginCSSGeopePortalLightClasses = $server + "geope\classes.css"
-        $pathOriginCSSGeopeGrid = $server + "geope\grid.css"
-
-        if ( Test-Path -Path $pathOriginCSSGeopeResourceClasses ) {
-
-            if ( Test-Path -Path $pathDestinyCSSResourceClasses ) {
-                Remove-Item -path $pathDestinyCSSResourceClasses -recurse
-            }
-
-            Copy-Item $pathOriginCSSGeopeResourceClasses -Destination $pathDestinyCSSResource
-        }
-
-        if ( Test-Path -Path $pathOriginCSSGeopePortalLightClasses ) {
-        
-            if ( Test-Path -Path $pathDestinyCSSPortalLightClasses ) {
-                Remove-Item -path $pathDestinyCSSPortalLightClasses -recurse
-            }
-
-            Copy-Item $pathOriginCSSGeopePortalLightClasses -Destination $pathDestinyCSSPortalLight
-        }
-
-        if ( Test-Path -Path $pathOriginCSSGeopeGrid ) {
-
-            if ( Test-Path -Path $pathdestinyCSSHTMLGridFile ) {
-                Remove-Item -path $pathdestinyCSSHTMLGridFile -recurse
-            }
-
-            Copy-Item $pathOriginCSSGeopeGrid -Destination $pathDestinyCSSHTMLGrid  
-        } 
-    }
-
+function show-message-success
+{
     Write-Host ""
     Write-Host "***********************************"
     Write-Host "*                                 *"
@@ -165,5 +70,175 @@ DO {
     Write-Host "*                                 *"
     Write-Host "***********************************"
     Write-Host ""
+}
+
+function show-message-error
+{
+    Write-Host ""
+    Write-Host "***********************************"
+    Write-Host "*                                 *"
+    Write-Host "*     Operação não realizada!     *"
+    Write-Host "*                                 *"
+    Write-Host "***********************************"
+    Write-Host ""
+}
+
+function service-on
+{
+    Param([String]$serviceName)
+
+    $arrService = Get-Service -Name $serviceName
+
+    if ( $arrService.Status -eq 'Running' ) {
+        Write-Host ""
+        Write-Host "***********************************"
+        Write-Host "*                                 *"
+        Write-Host "*     Serviço já está ativo!      *"
+        Write-Host "*                                 *"
+        Write-Host "***********************************"
+        Write-Host ""
+    }
+
+    while ($arrService.Status -ne 'Running')
+    {
+        Start-Service $serviceName
+
+        write-host $arrService.status
+        write-host 'Iniciando Serviço...'
+
+        $arrService.Refresh()
+
+        if ( $arrService.Status -eq 'Running' ) {
+
+            write-host $arrService.status
+
+            show-message-success
+        }
+    }
+}
+
+function service-off
+{
+    Param([String]$serviceName)
+
+    $arrService = Get-Service -Name $ServiceName
+
+    if ( $arrService.Status -eq 'Stopped' ) {
+        Write-Host ""
+        Write-Host "***********************************"
+        Write-Host "*                                 *"
+        Write-Host "*     Serviço já está parado!     *"
+        Write-Host "*                                 *"
+        Write-Host "***********************************"
+        Write-Host ""
+    }
+
+    while ($arrService.Status -ne 'Stopped')
+    {
+        Stop-Service $ServiceName
+
+        write-host $arrService.status
+        write-host 'Finalizando Serviço...'
+
+        $arrService.Refresh()
+
+        if ( $arrService.Status -eq 'Stopped' ) {
+            write-host $arrService.status
+            show-message-success
+        }
+    }
+}
+
+DO {
+    $action = Read-Host -Prompt "Digite: (1) Parar WebRun | (2) Iniciar WebRun | (3) Clear cache | (4) CSS CityBus | (5) CSS Financeiro | (6) CSS Geope | (0) Sair"
+
+    if ( $action -eq "1" ) {
+    	try {
+        	service-off -serviceName "Webrun_Studio"
+        }
+        catch {
+        	show-message-error
+        }	
+    }
+
+    if ( $action -eq "2" ) {
+    	try {
+        	service-on -serviceName "Webrun_Studio"
+        }
+        catch {
+        	show-message-error
+        }	
+    }
+
+    if ( $action -eq "3" ) {
+    	try {
+	        $pathSaved = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\config\saved\*"
+	        $pathCatalina = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\work\Catalina"
+	        $pathCache = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\cache\*"
+	        $pathWebrunstudio = "C:\Program Files (x86)\Softwell Solutions\Maker Studio\Webrun Studio\tomcat\webapps\webrunstudio\cacheCompressed\webrunstudio\*"
+
+	        delete-file-or-directory-if-exists -fileOrDirectory $pathCatalina
+
+	        clean-directory -directory $pathSaved
+	        clean-directory -directory $pathCache
+	        clean-directory -directory $pathWebrunstudio
+
+	        show-message-success
+	    }
+	    catch {
+	    	show-message-error
+	    }    
+    }
+
+    if ( $action -eq "4" ) {
+    	try {
+	        $pathOriginCSSCityBusResourceClasses = $server + "citybus\resource\classes.css"
+	        $pathOriginCSSCityBusPortalLightClasses = $server + "citybus\classes.css"
+	        $pathOriginCSSCityBusGrid = $server + "citybus\grid.css"
+
+	        copy-file-to-directory -newFile $pathOriginCSSCityBusResourceClasses -destinationDirectory $pathDestinyCSSResource -oldFile $pathDestinyCSSResourceClasses
+	        copy-file-to-directory -newFile $pathOriginCSSCityBusPortalLightClasses -destinationDirectory $pathDestinyCSSPortalLight -oldFile $pathDestinyCSSPortalLightClasses
+	        copy-file-to-directory -newFile $pathOriginCSSCityBusGrid -destinationDirectory $pathDestinyCSSHTMLGrid -oldFile $pathdestinyCSSHTMLGridFile
+
+	        show-message-success
+	    }
+	    catch {
+	    	show-message-error
+	    }    
+    }
+
+    if ( $action -eq "5" ) {
+    	try {
+	        $pathOriginCSSFinanceiroResourceClasses = $server + "financeiro\resource\classes.css"
+	        $pathOriginCSSFinanceiroPortalLightClasses = $server + "financeiro\classes.css"
+	        $pathOriginCSSFinanceiroGrid = $server + "financeiro\grid.css"
+
+	        copy-file-to-directory -newFile $pathOriginCSSFinanceiroResourceClasses -destinationDirectory $pathDestinyCSSResource -oldFile $pathDestinyCSSResourceClasses
+	        copy-file-to-directory -newFile $pathOriginCSSFinanceiroPortalLightClasses -destinationDirectory $pathDestinyCSSPortalLight -oldFile $pathDestinyCSSPortalLightClasses
+	        copy-file-to-directory -newFile $pathOriginCSSFinanceiroGrid -destinationDirectory $pathDestinyCSSHTMLGrid -oldFile $pathdestinyCSSHTMLGridFile
+
+	        show-message-success
+	    }
+	    catch {
+	    	show-message-error
+	    }    
+    }
+
+    if ( $action -eq "6" ) {
+    	try {
+	        $pathOriginCSSGeopeResourceClasses = $server + "geope\resource\classes.css"
+	        $pathOriginCSSGeopePortalLightClasses = $server + "geope\classes.css"
+	        $pathOriginCSSGeopeGrid = $server + "geope\grid.css"
+
+	        copy-file-to-directory -newFile $pathOriginCSSGeopeResourceClasses -destinationDirectory $pathDestinyCSSResource -oldFile $pathDestinyCSSResourceClasses
+	        copy-file-to-directory -newFile $pathOriginCSSGeopePortalLightClasses -destinationDirectory $pathDestinyCSSPortalLight -oldFile $pathDestinyCSSPortalLightClasses
+	        copy-file-to-directory -newFile $pathOriginCSSGeopeGrid -destinationDirectory $pathDestinyCSSHTMLGrid -oldFile $pathdestinyCSSHTMLGridFile
+
+	        show-message-success
+	    }
+	    catch {
+	    	show-message-error
+	    }    
+    }
 
 } While ( $action -ne "0" )
